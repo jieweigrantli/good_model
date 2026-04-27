feb0d17fe8d42e057f61e940d7a4bc8f4904a219
jieweigrantli <jwgli@ucdavis.edu>
2026-04-27 08:18:19 -0700

    Update 01_01_get_LDV_households_per_TAZ.py

--- Distribution-Grid-EV-CA/01_01_get_LDV_households_per_TAZ.py
+++ Distribution-Grid-EV-CA/01_01_get_LDV_households_per_TAZ.py
@@ -22,10 +22,10 @@ from common import fread_all_in_dir
 # ---------------------------------------------------------------------------
 WORK_ROOT = r"E:\GitHub\good_model\Distribution-Grid-EV-CA"
 
-SDPTM_DIR = r"E:\Data\CSTDM_OD/SDPTM"
-LDPTM_TRIPS = r"E:\Data\CSTDM_OD/LDPTM/LDPTM_Trips.csv"
-LDPTM_ACCEGR = r"E:\Data\CSTDM_OD/LDPTM/LDPTM_AccEgr.csv"
-ETM_TRIPS = r"E:\Data\CSTDM_OD/ETM/trips_Ext.csv"
+SDPTM_DIR = r"E:\Data\CSTDM_OD\SDPTM"
+LDPTM_TRIPS = r"E:\Data\CSTDM_OD\LDPTM\LDPTM_Trips.csv"
+LDPTM_ACCEGR = r"E:\Data\CSTDM_OD\LDPTM\LDPTM_AccEgr.csv"
+ETM_TRIPS = r"E:\Data\CSTDM_OD\ETM\trips_Ext.csv"
 
 OUT_DIR = r"E:\GitHub\good_model\Distribution-Grid-EV-CA\data\mobility_data\CSTDM_processed"
 LDV_MODES = {"SOV", "HOV2", "HOV3"}
